f0a64ae2eeed2943e495696a4b00f54a66bbda2e
risal-kv <risal@keyvalue.systems>
2025-07-22 19:08:16 +0530

    Calendar feature

--- be/routes/__init__.py
+++ be/routes/__init__.py
@@ -8,6 +8,7 @@ from routes.dashboards import router as dashboards_router
 from routes.integrations import router as integrations_router
 from routes.news import router as news_router
 from routes.health import router as health_router
+from routes.calendar import router as calendar_router
 
 # Create a main router to include all other routers
 api_router = APIRouter()
@@ -18,3 +19,4 @@ api_router.include_router(dashboards_router)
 api_router.include_router(integrations_router)
 api_router.include_router(news_router)
 api_router.include_router(health_router)
+api_router.include_router(calendar_router)
